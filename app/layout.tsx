c7bfc4e1ffc4bc5e7f77e19b351e39cccd59875b
Juan Manuel Zarza <info@cswingenieriacivil.com>
2025-07-28 21:24:18 -0300

    Fix large white space issue by removing min-h-screen and flex-1

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,7 +94,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="min-h-screen flex flex-col">
           <Header />
-          <main className="flex-1">
+          <main>
             {children}
           </main>
           <Footer />
